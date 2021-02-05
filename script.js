b6a6457c7167fa168dfcfc75f553dd2deeb85100
Marcin Kwiatek <kwiatekm4@gmail.com>
2021-02-05 18:30:50 +0100

    add inactive button

--- script.js
+++ script.js
@@ -6,16 +6,40 @@ br.addEventListener("click", function () { change(1) })
 
 let picture = 1
 
-function change(nr){
-    if(picture===1 && nr===0 || picture===5 && nr===1){return 0}
-    if(nr===1){
+function change(nr) {
+    if (picture === 1 && nr === 0 || picture === 5 && nr === 1) {
+        return 0
+    }
+    if (nr === 1) {
         let nrImage = picture + 1
-        document.getElementById("img").src="img/"+nrImage+".jpg"
+        document.getElementById("img").src = "img/" + nrImage + ".jpg"
         picture++
     }
-    if(nr===0){
+    if (nr === 0) {
         let nrImage = picture - 1
-        document.getElementById("img").src="img/"+nrImage+".jpg"
+        document.getElementById("img").src = "img/" + nrImage + ".jpg"
         picture--
     }
+    inactive()
 }
+function inactive() {
+    if (picture === 1) {
+        let name = document.getElementById("leftButton")
+        name.className = "inactiveButton"
+    }
+    if (picture !== 1) {
+        let name = document.getElementById("leftButton")
+        name.className = "sliderButton"
+    }
+    if (picture === 5) {
+        let name = document.getElementById("rightButton")
+        name.className = "inactiveButton"
+    }
+    if (picture !== 5) {
+        let name = document.getElementById("rightButton")
+        name.className = "sliderButton"
+    }
+}
+document.addEventListener("DOMContentLoaded", function () {
+    inactive()
+})
